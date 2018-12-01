6983379bf974b277035bd65447c84f012c35e77d
xbexbex <petja.valkama@gmail.com>
2018-12-01 16:35:06 +0200

    add instrument

--- src/components/Instrument.tsx
+++ src/components/Instrument.tsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
 import '../styles/instrument.scss'
+import { VictoryLine } from 'victory';
+import axios from 'axios';
+import { serverUrl } from '../utils/config';
 
 export default class Instrument extends Component {
 
     constructor(props: {}) {
         super(props)
+        this.state = {
+            chartData: null
+        }
+    }
+
+    async fetchChartData() {
+        try {
+            const response = await axios.get(serverUrl + '/funds/' + 'FI4000029509');
+            console.log(response.data)
+            this.setState({
+                chartData: response.data.payload,
+            });
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    componentDidMount() {
+        this.fetchChartData();
     }
 
     render() {
@@ -12,6 +34,12 @@ export default class Instrument extends Component {
             <div className="instrument">
                 <h4>{this.props.data.name}</h4>
                 <p>Value: {this.props.data.value}</p>
+                <p>Isin Code: {this.props.data.isinCode}</p>
+                {this.state.chartData && (
+                    <div className="chart-container">
+                        <VictoryLine></VictoryLine>
+                    </div>
+                )}
             </div>
         )
     }
